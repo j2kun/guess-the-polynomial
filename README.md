cb2a67968da802671cb10e46c8dee42179210076
Jeremy Kun <j2kun@users.noreply.github.com>
2018-02-14 07:24:59 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Guess your polynomial
+
+Secretly pick a polynomial with nonnegative integer coefficients. Then answer
+just two questions and watch as your polynomial is read back to you!
+
+```
+$ python guess.py  
+What is f(1)? 4
+What is f(5)? 56
+Your polynomial is: 
+
+	1 + 1 x^1 + 2 x^2
+
+```
---
+++ guess.py
@@ -0,0 +1,18 @@
+from polynomial import Polynomial
+
+
+answer = input("What is f(1)? ")
+sum_of_coeffs = int(answer)
+N = 1 + sum_of_coeffs
+
+answer = input("What is f({})? ".format(N))
+y = int(answer)
+
+coeffs = []
+while y % N != 0:
+    coeff = y % N
+    coeffs.append(int(coeff))
+    y = (y - coeff) / N
+
+p = Polynomial(coeffs)
+print("Your polynomial is: \n\n\t{}\n".format(p))
